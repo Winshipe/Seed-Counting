e1c383099b1bf5af2f0a34d31bddf7802b45b269
Winshipe <winshipea@gmail.com>
2016-07-06 15:17:15 -0400

    Added functionality to pull_out_ovals.py
    Made pull_out_ovals automatically update the macro with oval changes

---
+++ pull_out_ovals2.py
@@ -0,0 +1,23 @@
+import sys
+new_ovals = ''
+file1 = open(sys.argv[1],'r')
+for line in file1:
+	if '//' in line:
+		for char in line:
+			if (char.isdigit()) or (char==','):
+				new_ovals+=char
+		new_ovals+=','
+file2 = open("7_small_plate_partial_macro.ijm",'r')
+buffer = ''
+for line in file2:
+    if "newArray" not in line:
+        buffer+=line
+    else:
+        buffer+=('ovals = newArray('+new_ovals[:-1]+');\n')
+file2.close()
+file3 = open("7_small_plate_partial_macro.ijm",'w')
+file3.write(buffer)
+#file2 = open('extracted_ovals'+sys.argv[1][-5]+'.txt','w')
+#file2.write(new_ovals)
+file1.close()
+file3.close()
