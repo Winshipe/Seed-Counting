d449a613bedab5e4be05ecb75c0edf37ad0347df
Winshipe <winshipea@gmail.com>
2016-07-08 16:07:40 -0400

    Added functionality to pull_out_ovals2.py
    Gave it the ability to modify the scale of the macro as well as the target regions

--- pull_out_ovals2.py
+++ pull_out_ovals2.py
@@ -10,8 +10,11 @@ for line in file1:
 file2 = open("3_small_plate_partial_macro.ijm",'r')
 buffer = ''
 for line in file2:
-    if "newArray" not in line:
+    if "newArray" not in line or ("Set Scale" not in line and sys.argv[2]):
         buffer+=line
+    elif "Set Scale" in line and sys.argv[2]:
+        dist = 0.04*int(sys.argv[2])*60###0.04 px/(DPI*mm^2)*x DPI*60mm = y px/mm e.g. 500 DPI -> 20 px/mm
+        buffer+=('run("Set Scale...","distance='+dist+' known=60 unit=unit");')
     else:
         buffer+=('ovals = newArray('+new_ovals[:-1]+');\n')
 file2.close()
