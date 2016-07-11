c52c46eca5902b324df1af6b6f3e9bf98ef49401
Winshipe <winshipea@gmail.com>
2016-07-11 14:11:49 -0400

    Added safety features

--- pull_out_ovals2.py
+++ pull_out_ovals2.py
@@ -27,9 +27,12 @@ for line in file1:
 			if (char.isdigit()) or (char==','):
 				n_ovals+=char
 		n_ovals+=','
-file2 = open("3_small_plate_partial_macro.ijm",'r')
-buffer = edit_file(file2,n_ovals)
-file2.close()
+try:
+	file2 = open("3_small_plate_partial_macro.ijm",'r')
+	buffer = edit_file(file2,n_ovals)
+	file2.close()
+except IOError:
+	buffer = None
 try:
     assert buffer
     file3 = open("3_small_plate_partial_macro.ijm",'w')
@@ -37,9 +40,12 @@ try:
     file3.close()
 except AssertionError:
     print("Failed to make buffer for macro 3!")
-file4 = open("4_small_plate_partial_macro.ijm",'r')
-buffer = edit_file(file4,n_ovals)
-file4.close()
+try:
+	file4 = open("4_small_plate_partial_macro.ijm",'r')
+	buffer = edit_file(file4,n_ovals)
+	file4.close()
+except IOError:
+	buffer = None
 try:
     assert buffer
     file5 = open("4_small_plate_partial_macro.ijm",'w')
