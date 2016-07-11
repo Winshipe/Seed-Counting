9045ecd9c59f6439a9a41a8297ac89349eaa1805
Winshipe <winshipea@gmail.com>
2016-07-11 14:08:21 -0400

    Added safety features

--- pull_out_ovals2.py
+++ pull_out_ovals2.py
@@ -1,26 +1,50 @@
 import sys
-new_ovals = ''
+def edit_file(file,new_ovals):
+    try:    
+        assert new_ovals
+    except AssertionError:
+        print('No Ovals!')
+    try:
+        assert file
+    except AssertionError:
+        print("No file!")
+    buffer = ''
+    for line in file:
+        if "newArray" in line:
+            buffer+=('ovals = newArray('+new_ovals[:-1]+');\n')
+        elif "Set Scale" in line and sys.argv[2]:
+            dist = 0.04*int(sys.argv[2])*60###0.04 px/(DPI*mm^2)*x DPI*60mm = y px/mm e.g. 500 DPI -> 20 px/mm
+            buffer+=('\t\trun("Set Scale...","distance='+str(dist)+' known=60 unit=unit");')
+        else:
+            buffer+=line
+#    print(buffer.split('\n')[:2])
+    return buffer
+n_ovals = ''
 file1 = open(sys.argv[1],'r')
 for line in file1:
-	if '//' in line:
+	if '**' in line:
 		for char in line:
 			if (char.isdigit()) or (char==','):
-				new_ovals+=char
-		new_ovals+=','
+				n_ovals+=char
+		n_ovals+=','
 file2 = open("3_small_plate_partial_macro.ijm",'r')
-buffer = ''
-for line in file2:
-    if "newArray" not in line or ("Set Scale" not in line and sys.argv[2]):
-        buffer+=line
-    elif "Set Scale" in line and sys.argv[2]:
-        dist = 0.04*int(sys.argv[2])*60###0.04 px/(DPI*mm^2)*x DPI*60mm = y px/mm e.g. 500 DPI -> 20 px/mm
-        buffer+=('run("Set Scale...","distance='+dist+' known=60 unit=unit");')
-    else:
-        buffer+=('ovals = newArray('+new_ovals[:-1]+');\n')
+buffer = edit_file(file2,n_ovals)
 file2.close()
-file3 = open("3_small_plate_partial_macro.ijm",'w')
-file3.write(buffer)
-#file2 = open('extracted_ovals'+sys.argv[1][-5]+'.txt','w')
-#file2.write(new_ovals)
+try:
+    assert buffer
+    file3 = open("3_small_plate_partial_macro.ijm",'w')
+    file3.write(buffer)
+    file3.close()
+except AssertionError:
+    print("Failed to make buffer for macro 3!")
+file4 = open("4_small_plate_partial_macro.ijm",'r')
+buffer = edit_file(file4,n_ovals)
+file4.close()
+try:
+    assert buffer
+    file5 = open("4_small_plate_partial_macro.ijm",'w')
+    file5.write(buffer)
+    file5.close()
+except AssertionError:
+    print("Failed to make buffer for macro 4!")
 file1.close()
-file3.close()
